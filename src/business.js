22f72cc2a4a180728f5389701d9ae1f57cb2e0bc
lm <leo.mangutov@gmail.com>
2019-03-15 17:11:48 -0700

    add function to calculate age on different planets, and test the specs

---
+++ src/business.js
@@ -0,0 +1,18 @@
+
+
+function ageOnMercury(age) {
+  const mercuryAge = (age * 365) / 88;
+  return mercuryAge;
+}
+function ageOnVenus(age) {
+  const venusAge = (age * 365) / 88;
+  return venusAge;
+}
+function ageOnMars(age) {
+  const marsAge = (age * 365) / 88;
+  return marsAge;
+}
+function ageOnJupiter(age) {
+  const jupiterAge = (age * 365) / 88;
+  return jupiterAge;
+}
