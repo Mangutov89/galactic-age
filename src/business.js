3cbaa9668e265a0f39318d06f916feb369002b49
lm <leo.mangutov@gmail.com>
2019-03-17 18:26:19 -0700

    changed the function to a class constructor

--- src/business.js
+++ src/business.js
@@ -1,18 +1,53 @@
+export class GalacticAge {
+  constructor(age) {
+    this.age = age;
+  }
+  ageOnMercury() {
+    const mercuryAge = (this.age * 365) / 88;
+    return mercuryAge;
+  }
+  ageOnVenus() {
+    const venusAge = (this.age * 365) / 88;
+    return venusAge;
+  }
+  ageOnMars() {
+    const marsAge = (this.age * 365) / 88;
+    return marsAge;
+  }
+  ageOnJupiter() {
+    const jupiterAge = (this.age * 365) / 88;
+    return jupiterAge;
+  }
+}
 
 
-function ageOnMercury(age) {
-  const mercuryAge = (age * 365) / 88;
-  return mercuryAge;
-}
-function ageOnVenus(age) {
-  const venusAge = (age * 365) / 88;
-  return venusAge;
-}
-function ageOnMars(age) {
-  const marsAge = (age * 365) / 88;
-  return marsAge;
-}
-function ageOnJupiter(age) {
-  const jupiterAge = (age * 365) / 88;
-  return jupiterAge;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function ageOnMercury(age) {
+//   const mercuryAge = (age * 365) / 88;
+//   return mercuryAge;
+// }
+// function ageOnVenus(age) {
+//   const venusAge = (age * 365) / 88;
+//   return venusAge;
+// }
+// function ageOnMars(age) {
+//   const marsAge = (age * 365) / 88;
+//   return marsAge;
+// }
+// function ageOnJupiter(age) {
+//   const jupiterAge = (age * 365) / 88;
+//   return jupiterAge;
+// }
